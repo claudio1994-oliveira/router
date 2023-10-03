3c62637926741fc715575abd71d6c241d44129b5
Cláudio Oliveira <franciscoclaudiooliveira@gmail.com>
2023-10-03 13:32:48 -0300

    commit inicial

---
+++ tests/Router/RouterTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Claud\Router\Test\Router;
+
+use PHPUnit\Framework\TestCase;
+
+class RouterTest extends TestCase
+{
+    public function testRouter()
+    {
+        $this->assertTrue(true);
+    }
+}
